11f2e583caa51cde9f72bd59ccaac8e470a7a1a1
gi❤️ <giovannaschreiner15@gmail.com>
2026-02-12 09:09:14 -0300

    Add files via upload

---
+++ atv7/script.js
@@ -0,0 +1,9 @@
+function modoDark() {
+    document.body.style.backgroundColor = "#121212";
+    document.body.style.color = "#f0f0f0";
+}
+
+function modoLight() {
+    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.color = "#000000";
+}
